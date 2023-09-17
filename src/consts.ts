678ef190ec68e357b441e449c3fb0e88f8c518ea
Sam Perry <samperry.dev@gmail.com>
2023-09-17 13:16:16 +0100

    wip

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sam\'s Blog';
+export const SITE_TITLE = 'Sam\'s thoughts and ramblings';
 export const SITE_DESCRIPTION = 'Sam perry, full stack developer code monkey and occasional picker uper of heavy things.';
