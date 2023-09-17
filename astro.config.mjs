c1d7b4876e75fddc75edcb9b7d33328cdc93b3de
Sam Perry <samperry.dev@gmail.com>
2023-09-17 22:46:07 +0100

    testt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
+import netlify from "@astrojs/netlify/functions";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  output: "server",
+  server: {
+      headers: {
+          "Access-Control-Allow-Origin": "https://identity.netlify.com/v1/netlify-identity-widget.js"
+      }
+  },
+  adapter: netlify(),
+  site: 'https://fiendtech.com',
   integrations: [mdx(), sitemap(), tailwind()]
 });
